0e6d822c50a1ccb5e253fc782977e9216fd28ec8
andmohiko <andmohiko@gmail.com>
2022-12-14 02:05:30 +0900

    fix: push in useeffect

--- src/components/Layouts/DefaultLayout.tsx
+++ src/components/Layouts/DefaultLayout.tsx
@@ -1,4 +1,6 @@
-import { ReactElement, ReactNode } from 'react'
+import { ReactElement, ReactNode, useEffect } from 'react'
+
+import { useRouter } from 'next/router'
 
 import { PageHead } from '~/components/Head'
 
@@ -6,16 +8,27 @@ type Props = {
   children?: ReactNode
 }
 
-export const DefaultLayout = ({ children }: Props): ReactElement => (
-  <div>
-    <PageHead />
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-      }}
-    >
-      {children}
+export const DefaultLayout = ({ children }: Props): ReactElement => {
+  const { push } = useRouter()
+  const user = null
+
+  useEffect(() => {
+    if (!user) {
+      push('/new')
+    }
+  }, [push])
+
+  return (
+    <div>
+      <PageHead />
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+        }}
+      >
+        {children}
+      </div>
     </div>
-  </div>
-)
+  )
+}
