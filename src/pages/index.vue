c54a4a4917af6ddf161d61a4aaa41a78a188a791
andmohiko <andmohiko@gmail.com>
2022-12-16 20:23:09 +0900

    feat: global header

---
+++ src/pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <FlexBox class="container">
+    <p class="text">ああああ</p>
+  </FlexBox>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
+
+<style lang="scss" scoped>
+.container {
+  display: flex;
+  width: 100%;
+  border: 2px solid red;
+}
+.text {
+  font-size: 20px;
+  color: blue;
+}
+</style>
